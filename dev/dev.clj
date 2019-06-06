eec83f5a711b2a18460e33fee6caf3ed8b3fadf1
Jeroen de Jong <thumbnail@users.noreply.github.com>
2019-06-06 08:37:24 +0200

    Sync project setup with that of our Lein template

--- dev/dev.clj
+++ dev/dev.clj
@@ -7,8 +7,9 @@
    [clojure.test :refer [run-all-tests run-tests]]
    [clojure.tools.namespace.repl :refer [refresh set-refresh-dirs]]
    [criterium.core :refer [quick-bench]]
-   [formatting-stack.branch-formatter :refer [format-and-lint-branch!]]
-   [formatting-stack.project-formatter :refer [format-and-lint-project!]]
+   [formatting-stack.branch-formatter :refer [format-and-lint-branch! lint-branch!]]
+   [formatting-stack.compilers.test-runner :refer [test!]]
+   [formatting-stack.project-formatter :refer [format-and-lint-project! lint-project!]]
    [lambdaisland.deep-diff]
    [nedap.utils.modular.api :refer :all]))
 
@@ -30,3 +31,9 @@
   (-> x
       (lambdaisland.deep-diff/diff y)
       (lambdaisland.deep-diff/pretty-print)))
+
+(defn gt
+  "gt stands for git tests"
+  []
+  (refresh)
+  (test!))
