a1605f9d44fbfc001b986542a2160848bd2693d7
vemv <vemv@users.noreply.github.com>
2019-07-15 04:24:00 +0200

    `implement`: add coverage for multimethods
    Fixes nedap/utils.modular#3

--- test/unit/nedap/utils/modular/api/implement.clj
+++ test/unit/nedap/utils/modular/api/implement.clj
@@ -5,6 +5,13 @@
    [nedap.utils.modular.api :as sut]
    [unit.nedap.utils.modular.api.example-external-protocol :as example-external-protocol]))
 
+(def this-ns *ns*)
+
+(defn aliased? [s]
+  (->> (ns-aliases this-ns)
+       (keys)
+       (some #{s})))
+
 (defprotocol Thing
   :extend-via-metadata true
   (foo [this x]))
@@ -16,24 +23,37 @@
 (defn foo-impl [this x]
   (+ x x))
 
+(defmulti foo-alternative-impl (constantly :foo-alternative-impl))
+
+(defmethod foo-alternative-impl :foo-alternative-impl [this x]
+  (* x x))
+
 (def not-an-impl 2)
 
 (deftest implement
   (testing "metadata is associated, and ns-qualifed"
-    (is (= foo-impl (-> (sut/implement {}
-                          foo foo-impl)
-                        (meta)
-                        (get `foo)))))
+    (are [impl] (= impl
+                   (-> (sut/implement {}
+                         foo impl)
+                       (meta)
+                       (get `foo)))
+      foo-impl
+      foo-alternative-impl))
 
   (testing "The protocol function can be invoked"
-    (is (= 42 (-> (sut/implement {}
-                    foo foo-impl)
-                  (foo 21)))))
+    (are [impl result] (= result (-> (sut/implement {}
+                                       foo impl)
+                                     (foo 21)))
+      foo-impl             42
+      foo-alternative-impl 441))
 
   (testing "swapped order"
-    (with-out-str
-      (is (thrown? Exception (sut/implement {}
-                               foo-impl foo)))))
+    (are [impl] (with-out-str
+                  (is (thrown? Exception (sut/implement {}
+                                           impl foo))))
+
+      foo-impl
+      foo-alternative-impl))
 
   (testing "values that don't resolve to a function"
     (with-out-str
@@ -41,11 +61,20 @@
                                foo not-an-impl)))))
 
   (testing "made-up symbols"
+
+    ;; no `are` here since `eval '` makes it harder to predict what will actually happen
     (with-out-str
       (is (thrown? Exception (eval '(sut/implement {}
-                                      foo-impl oooooommmmmggggg))))))
+                                      foo-impl oooooommmmmggggg)))))
 
-  (testing "spurious aliases in either side of the mapping"
+    (with-out-str
+      (is (thrown? Exception (eval '(sut/implement {}
+                                      foo-alternative-impl oooooommmmmggggg))))))
+
+  (testing "Spurious aliases in either side of the mapping"
+    (assert (aliased? 'sut))
+    (assert (not (aliased? 'made)))
+
     (with-out-str
       (is (thrown? Exception (eval '(sut/implement {}
                                       made/up inc)))))
@@ -55,9 +84,11 @@
                                       foo made/up))))))
 
   (testing "Implementing incompatible protocols is prevented"
-    (is (thrown-with-msg? Exception #"The targeted protocol does not have `:extend-via-metadata` activated."
-                          (sut/implement {}
-                            bar foo-impl))))
+    (are [impl] (thrown-with-msg? Exception #"The targeted protocol does not have `:extend-via-metadata` activated."
+                                  (sut/implement {}
+                                    bar impl))
+      foo-impl
+      foo-alternative-impl))
 
   (testing "Specifyicing protocols (and implementations) belonging to other namespaces, their symbols having an alias prefix"
     (is (= 3000
