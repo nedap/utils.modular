a0bd69db6d0ed3e1361d0ef3c22930bb6bf15f91
vemv <vemv@users.noreply.github.com>
2020-02-04 20:21:24 +0100

    Upgrade speced.def

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 ;; Please don't bump the library version by hand - use ci.release-workflow instead.
 (defproject com.nedap.staffing-solutions/utils.modular "2.1.0-alpha3"
   ;; Please keep the dependencies sorted a-z.
-  :dependencies [[com.nedap.staffing-solutions/speced.def "1.2.0-alpha1"]
+  :dependencies [[com.nedap.staffing-solutions/speced.def "2.0.0"]
                  [com.stuartsierra/component "0.4.0"]
                  [org.clojure/clojure "1.10.1"]]
 
